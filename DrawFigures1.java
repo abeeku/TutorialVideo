0a669b8d4bfa11f30bf8d1b9e7f5dc3cc7454a43
unknown <Administrator@B219-014.techhigh.local>
2014-09-22 14:07:32 -0400

    Added Draw Figures Java file

---
+++ DrawFigures1.java
@@ -0,0 +1,11 @@
+public class DrawFigures1 {
+	public static void main(String args[]) {
+		System.out.println(" _________");
+		System.out.println("|_________|");
+		System.out.println("|_________|");
+		System.out.println("|_________|");
+		System.out.println("|_________|");
+		
+		
+	}
+}
