1b84f07d185c7f8baf5332e20a9e84c66d9743ab
Abeeku Bondzie <kwekubondzie@gmail.com>
2014-09-22 14:16:12 -0400

    Added Proc. Decomp.

--- DrawFigures1.java
+++ DrawFigures1.java
@@ -1,12 +1,16 @@
 public class DrawFigures1 {
 // Add a comment right here
 	public static void main(String args[]) {
+		square();
+		
+	}
+	
+	public static void square() {
 		System.out.println(" _________");
 		System.out.println("|_________|");
 		System.out.println("|_________|");
 		System.out.println("|_________|");
 		System.out.println("|_________|");
-		
 		
 	}
 }
